9a5ae250221a98f1c23aa9d4c851fc659f8f043d
Thawee <thaweemail@gmail.com>
2012-05-10 17:08:00 +0700

    delete ics

--- build_m9/build.sh
+++
@@ -1,7 +0,0 @@
-make clean
-make apk
-version=`../tools/version.sh`
-read id date <<<$(IFS="-"; echo $version)
-rbversion="rockbox-m9-$date-$id.apk"
-echo "new version $rbversion"
-mv rockbox.apk $rbversion
